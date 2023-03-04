d145e8109704d959615530f34e23bf46dff11e7b
OlegMass <oleg.mass2015@yandex.ru>
2023-03-04 13:25:11 -0800

    Commit №3
    -add logic dodge and ignore_defense to the attack method;
    -add 2 versions of the battle showing

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ import random
 #         "defence"
 #         }
 
+
 class Creator:
     def __init__(self, life, defense, damage):
         self.level = 1
@@ -17,9 +18,20 @@ class Creator:
         self.defense = defense
         self.damage = damage
         self.critical = 10
+        self.dodge = 15
+        self.ignore_defense = 15
 
     def attack(self, user):
-        user.life -= self.damage - user.defense
+        if random.randint(1,100) <= self.dodge:
+            user.life -= 0
+        else:    
+            if random.randint(1, 100) <= self.critical:
+                if random.randint(1,100) <= self.ignore_defense:
+                    user.life -= self.damage*2 
+                user.life -= self.damage*2 - user.defense
+            if random.randint(1,100) <= self.ignore_defense:
+                user.life -= self.damage
+            user.life -= self.damage - user.defense
 
     def experiance(self):
         pass
@@ -27,41 +39,59 @@ class Creator:
 
 hero = Creator(100, 20, 30)
 
-creature = Creator(40, 10, 50)
+creature = Creator(40, 10, 30)
 
 
 def attack_creature():
+    
     creature_num = int(input("""
         How many Creatures do you want to attack?
     """))
-
-    while True:
-        input("Press enter to punch an enemy")
-        time.sleep(1)
-        hero.attack(creature)
-        if creature.life <= 0 and creature_num == 1:
-            print('You win!')
-            break
-        elif creature.life <= 0 and creature_num > 1:
-            creature_num -= 1
-            print(f"You kill 1 creature and {creature_num} creatures are left ")
-            creature.life = 40
-
-        print(f"Creature has {creature.life} life.")
-        time.sleep(1)
-        print("Creature attacking you too!!!")
-        for i in range(creature_num):
-            creature.attack(hero)
-            if hero.life <= 0:
-                print('You are dead!')
+    version = int(input("""
+    1 Show the details of a battle.
+    2 Don't show details of a battle
+    """))
+    life_one_creature = creature.life
+    if version == 1:
+        while True:
+            input("Press enter to punch an enemy")
+            time.sleep(1)
+            hero.attack(creature)
+            if creature.life <= 0 and creature_num == 1:
+                print('You win!')
                 break
-            
-            print(f"You have {hero.life} life.")
-            
-        if hero.life <= 0:
-            break
-        
-
+            elif creature.life <= 0 and creature_num > 1:
+                creature_num -= 1
+                print(f"You kill 1 creature and {creature_num} creatures are left ")
+                creature.life = life_one_creature
+            print(f"Creature has {creature.life} life.")
+            time.sleep(1)
+            print("Creature attacking you too!!!")
+            for i in range(creature_num):
+                creature.attack(hero)
+                if hero.life <= 0:
+                    print('You are dead!')
+                    break
+                print(f"You have {hero.life} life.")      
+            if hero.life <= 0:
+                break
+    elif version==2:
+        while True:
+            hero.attack(creature)
+            if creature.life <= 0 and creature_num == 1:
+                print('You win!')
+                break
+            elif creature.life <= 0 and creature_num > 1:
+                creature_num -= 1
+                creature.life = life_one_creature
+            for i in range(creature_num):
+                creature.attack(hero)
+                if hero.life <= 0:
+                    print('You are dead!')
+                    break                
+            if hero.life <= 0:
+                break
+    
 
 def main():
 
